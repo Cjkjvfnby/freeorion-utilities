d0b50c4a394333e2451818d4d5f40ef882957a18
Cjkjvfnby <Cjkjvfnby@gmail.com>
2015-07-11 16:58:14 +0300

    Add game class.

--- freeorion_dump_reader/reader/templatetags/items_tags.py
+++ freeorion_dump_reader/reader/templatetags/items_tags.py
@@ -4,6 +4,17 @@ from django.utils.safestring import mark_safe
 register = template.Library()
 
 
+
+@register.simple_tag()
+def game_label(empire):
+    return '<span>%s %s<small> [%s] </small><sup>%s</sup></span>' % (empire.name, empire.empire_id, empire.game_id, empire.creation_date.strftime('%d %b, %H:%M'))
+
+
+@register.simple_tag()
+def turn_label(turn):
+    return '<span>Turn %s<small> [%s] </small><sup>%s</sup></span>' % (turn.turn, turn.turn_id, turn.get_date().strftime('%d %b, %H:%M'))
+
+
 @register.filter(name='cell', is_safe=True, takes_context=True)
 def cell(value, key):
     if key == 'args':
