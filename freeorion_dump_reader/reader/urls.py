aab04f10f9b952fe38e5dc7d5b4101b3f0aa2159
Cjkjvfnby <Cjkjvfnby@gmail.com>
2015-07-11 15:02:19 +0300

    Refactoring. Remove game view.

--- freeorion_dump_reader/reader/urls.py
+++ freeorion_dump_reader/reader/urls.py
@@ -16,7 +16,6 @@ def get_url(*items):
 
 urlpatterns = [
     url(r'^$', views.GamesList.as_view(), name='games'),
-    url(get_url(GAME), views.GameView.as_view(), name='game'),
     url(get_url(GAME, TURN, SECTION, 'plot.png'), views.plot, name='plot'),
     url(get_url(GAME, SECTION, TURN), views.SectionView.as_view(), name='section'),
     url(get_url(GAME, SECTION, TURN, 'summary'), summaries.SummaryView.as_view(), name='summary'),
