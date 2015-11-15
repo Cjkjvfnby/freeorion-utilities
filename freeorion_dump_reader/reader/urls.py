f01c86ac0413d8ac32723bc6b0ca0a4e5589bb70
Cjkjvfnby <Cjkjvfnby@gmail.com>
2015-11-15 10:48:00 +0300

    Add some mixins and branch list view

--- freeorion_dump_reader/reader/urls.py
+++ freeorion_dump_reader/reader/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
 from . import views
+from . import importing
 
 SECTION = '(?P<section>\w+)'
-GAME = '(?P<game>\d+_[0-9a-f]+_\w+)'
-TURN = '(?P<turn>[0-9a-f]+)'
+GAME = '(?P<game_id>\d+_[0-9a-f]+_\w+)'
+TURN = '(?P<turn_id>[0-9a-f]+)'
 TURN_1 = '(?P<turn1>[0-9a-f]+)'
 TURN_2 = '(?P<turn2>[0-9a-f]+)'
 
@@ -15,8 +16,9 @@ def get_url(*items):
 
 urlpatterns = [
     url(r'^$', views.GameListView.as_view(), name='games'),
-    url(r'^import_list/$', views.ImportListView.as_view(), name='import_list'),
-    url(r'^import/$', views.ImportView.as_view(), name='import')
+    url(r'^import_list/$', importing.ImportListView.as_view(), name='import_list'),
+    url(r'^import/$', importing.ImportView.as_view(), name='import'),
+    url(get_url(GAME, TURN), views.BranchView.as_view(), name='branch'),
     # url(r'^$', views.GamesList.as_view(), name='games'),
     # url(r'^research_compare$', views.ResearchCompare.as_view(), name='research_compare'),
     # # url(get_url(GAME, SECTION, TURN, '(?P<start>\d+)', '(?P<end>\d+)', 'plot.png'), views.plot, name='plot'),
--- freeorion_dump_reader/reader/views.py
+++ freeorion_dump_reader/reader/views.py
@@ -1,98 +1,46 @@
-import json
-import os
-from django.db import transaction
-from django.http import HttpResponseRedirect
-from reader.models import Turn, Planet, Game, System, ResearchInfo, Research
+from reader.models import Game, Turn
 
-from django.views.generic import TemplateView, ListView, View
-from django.conf import settings
-from reader.tools import date_from_id
+from django.views.generic import TemplateView, ListView
 
 
 class GameListView(ListView):
     model = Game
 
 
-class ImportListView(TemplateView):
-    template_name = "reader/import.html"
+class ResearchInfo(TemplateView):
+    template_name = 'reader/research.html'
 
     def get_context_data(self, **kwargs):
-        context = super(ImportListView, self).get_context_data(**kwargs)
-        def is_dump(path):
-            # TODO add check for name style
-            return os.path.isdir(os.path.join(settings.DUMP_FOLDER, path))
-
-        games = sorted([os.path.join(settings.DUMP_FOLDER, path) for path in os.listdir(settings.DUMP_FOLDER) if is_dump(path)],
-                       key=os.path.getctime, reverse=True)
-
-        ids = [os.path.basename(path) for path in games]
-        present = Game.objects.all().in_bulk(ids)
-        context['games'] = [(game_id, game_id in present, path) for game_id, path in zip(ids, games)]
-        return context
+        pass
 
 
-def process_file(processor, game, name):
-    with open(name) as f:
-        for line in f:
-            data, items = json.loads(line)
-            turn, _ = Turn.objects.get_or_create(turn=data['turn'],
-                                                 turn_id=data['turn_id'],
-                                                 parent_id=data['parent_id'],
-                                                 game=game)
-
-            processor(game, turn, items)
+class GameMixin(TemplateView):
+    def get_context_data(self, **kwargs):
+        self.game = Game.objects.get(game_id=kwargs['game_id'])
+        kwargs['game'] = self.game
+        kwargs = self.prepare_context(**kwargs)
+        return super(GameMixin, self).get_context_data(**kwargs)
 
 
-def process_research_info(game, turn, items):
-    item = items[0]
-    ResearchInfo.objects.get_or_create(game=game,
-                                       type=item['type'],
-                                       category=item['category'],
-                                       name=item['name'],
-                                       cost=item['cost'])
+class TurenMixin(GameMixin):
+    def get_context_data(self, **kwargs):
+        self.turn = Turn.objects.get(turn_id=kwargs['turn_id'])
+        kwargs['turn'] = self.turn
+        return super(TurenMixin, self).get_context_data(**kwargs)
 
 
-def process_research(game, turn, items):
-    for item in items:
-        name = item.pop('name')
-        ri = ResearchInfo.objects.get(name=name, game=game)
-        Research.objects.get_or_create(turn=turn, research_info=ri, **item)
+class BranchView(TurenMixin):
+    template_name = 'reader/branch.html'
 
+    def prepare_context(self, **kwargs):
+        all_turns = Turn.objects.filter(turn__lte=self.turn.turn, game=self.game).order_by('-turn')
+        print len(all_turns)
 
-def process_systems(game, turn, items):
-    neighbors_dict = {}
-    systems = {}
-    for item in items:
-        neighbors_dict[item['sid']] = item.pop('neighbors')
-        system, _ = System.objects.get_or_create(turn=turn, **item)
-        systems[item['sid']] = system
-    for sid, neighbors in neighbors_dict.items():
-        for neighbor in neighbors:
-            systems[sid].neighbors.add(systems[neighbor])
-
-
-def process_planets(game, turn, items):
-    for item in items:
-        item['sid'] = System.objects.get(turn=turn, sid=item['sid'])
-        Planet.objects.get_or_create(turn=turn, **item)
-
-
-class ImportView(View):
-    @transaction.atomic
-    def post(self, request):
-        folder = request.POST['path']
-        game_id = os.path.basename(folder)
-        empire_id, creation_date, name = game_id.split('_', 2)
-
-        game, _ = Game.objects.get_or_create(
-            game_id=game_id,
-            empire_name=name,
-            empire_id=empire_id,
-            creation_date=date_from_id(creation_date))
-
-        process_file(process_research_info, game, os.path.join(folder, 'research_info'))
-        process_file(process_research, game, os.path.join(folder, 'research'))
-        process_file(process_systems, game, os.path.join(folder, 'system'))
-        process_file(process_planets, game, os.path.join(folder, 'planet'))
-
-        return HttpResponseRedirect('/')
+        branch = [self.turn]
+        next_turn = self.turn.parent_id
+        for item in all_turns:
+            if item.turn_id == next_turn:
+                branch.append(item)
+                next_turn = item.parent_id
+        kwargs['branch'] = reversed(branch)
+        return kwargs
