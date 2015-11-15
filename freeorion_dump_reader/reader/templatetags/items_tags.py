7fc16c2e62a005ea597b63caa3ca22effda6ce7f
Cjkjvfnby <Cjkjvfnby@gmail.com>
2015-11-15 11:12:51 +0300

    Fix item tags and use them in templates

--- freeorion_dump_reader/reader/templatetags/items_tags.py
+++ freeorion_dump_reader/reader/templatetags/items_tags.py
@@ -5,12 +5,13 @@ register = template.Library()
 
 
 @register.simple_tag()
-def game_label(empire):
-    return '<span>%s %s<small> [%s] </small><sup>%s</sup></span>' % (empire.name, empire.empire_id, empire.game_id, empire.creation_date.strftime('%d %b, %H:%M'))
+def game_label(game):
+    return '<span>%s %s<small> [%s] </small><sup>%s</sup></span>' % (game.empire_name, game.empire_id, game.game_id,
+                                                                     game.creation_date.strftime('%d %b, %H:%M'))
 
 
 @register.simple_tag()
 def turn_label(turn):
-    return '<span>Turn %s<small> [%s] </small><sup>%s</sup></span>' % (turn['turn'],
-                                                                       turn['turn_id'],
-                                                                       date_from_id(turn['turn_id']).strftime('%d %b, %H:%M'))
+    return '<span>Turn %s<small> [%s] </small><sup>%s</sup></span>' % (turn.turn,
+                                                                       turn.turn_id,
+                                                                       date_from_id(turn.turn_id).strftime('%d %b, %H:%M'))
